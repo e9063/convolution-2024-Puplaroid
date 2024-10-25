450d9fe13962f1f0c3ea1fd890af70b56cd02c84
Puplaroid <Aekkarach.su@ku.th>
2024-10-25 18:06:40 +0700

    test parallel

--- conv_parallel.c
+++ conv_parallel.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 
 int main() {
     int NA, NF;
@@ -8,11 +7,10 @@ int main() {
 
     int *A = malloc(sizeof(int) * NA);
     int *F = malloc(sizeof(int) * NF);
-    #pragma omp parallel for schedule(dynamic) num_threads(4)
+
     for (int i = 0; i < NA; i++) {
         scanf("%d", &A[i]);
     }
-    #pragma omp parallel for schedule(dynamic) num_threads(4)
     for (int i = 0; i < NF; i++) {
         scanf("%d", &F[i]);
     }
@@ -22,15 +20,13 @@ int main() {
     int *R = malloc(sizeof(int) * lenR);
 
 
-    #pragma omp parallel for schedule(dynamic) num_threads(4)
     for (int i = 0; i < lenR; i++) {
         int sum = 0;
         for (int j = 0; j < NF; j++) {
-            sum += A[i + j] * F[NF - 1 - j];
+            sum += A[i + j] * F[NF - 1 - j]; 
         }
         R[i] = sum;
     }
-
     for (int i = 0; i < lenR; i++) {
         printf("%d\n", R[i]);
     }
